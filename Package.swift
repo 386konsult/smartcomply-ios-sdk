4ea96a0e2efd7f61afe58aedf9d0bd81fc6f52ff
0xsherifdev <herdeybayor4real@gmail.com>
2026-08-24 15:00:59 +0100

    Release 1.0.2
    Populates verified name and ID number on the result screen (BUG-04).
    Verified on a device against production before release.

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SmartComplySDK",
-            url: "https://adhere-prod.s3.us-west-2.amazonaws.com/sdk-releases/ios/1.0.1/SmartComplySDK.xcframework.zip",
-            checksum: "a3b399d1a3bbe369f638fa6e7d1379b8baefe24a8df5bc0e3d6fe706a0bef040"
+            url: "https://adhere-prod.s3.us-west-2.amazonaws.com/sdk-releases/ios/1.0.2/SmartComplySDK.xcframework.zip",
+            checksum: "83a09d4bfc38cbb0212a889033216afb755227e99c9423cb6f7236834f4e7086"
         )
     ]
 )
